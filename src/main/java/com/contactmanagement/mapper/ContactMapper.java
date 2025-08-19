303cc33b594a9a8761a9b33a66c36b6d49817bc2
rsh <randrianirinahernandezstephan@gmail.com>
2025-08-19 03:03:35 +0300

    feat: mapper contactDTO to contact entity

---
+++ src/main/java/com/contactmanagement/mapper/ContactMapper.java
@@ -0,0 +1,30 @@
+package com.contactmanagement.mapper;
+
+import com.contactmanagement.entity.ContactEntity;
+import com.contactmanagement.entity.UserEntity;
+import com.contactmanagement.dto.ContactDTO;
+
+public class ContactMapper {
+
+    public static ContactDTO toDto(ContactEntity entity) {
+        return new ContactDTO(
+                entity.getId(),
+                entity.getFirstName(),
+                entity.getLastName(),
+                entity.getEmail(),
+                entity.getPhone(),
+                entity.getUser().getId()
+        );
+    }
+
+    public static ContactEntity toEntity(ContactDTO dto, UserEntity userEntity) {
+        return new ContactEntity(
+                dto.getFirstName(),
+                dto.getLastName(),
+                dto.getEmail(),
+                dto.getPhone(),
+                userEntity
+        );
+    }
+}
+
