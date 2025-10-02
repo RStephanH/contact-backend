a9a1f941702269b770abfababf6a6a0460a3f444
rsh <randrianirinahernandezstephan@gmail.com>
2025-10-02 12:32:05 +0300

    fix: asadmin script

--- config/setup-jdbc.sh
+++ config/setup-jdbc.sh
@@ -2,17 +2,18 @@
 
 # Wait for the default domain to be ready 
 sleep 20
+echo "AS_ADMIN_PASSWORD=admin" >> password.txt
 
-asadmin add-library ./mariadb-java-client-3.5.5.jar
+asadmin add-library --user admin --password-file=/opt/payara/password.txt ./mariadb-java-client-3.5.5.jar
 
 # Create JDBC connection pool
-asadmin create-jdbc-connection-pool \
+asadmin --user admin --password-file=/opt/payara/password.txt create-jdbc-connection-pool \
     --restype=javax.sql.DataSource \
     --datasourceclassname=org.mariadb.jdbc.MariaDbDataSource \
     --property user=contactuser:password=contactpass:Url=jdbc:mariadb://mariadb:3306/contactdb \
     ContactPool
 
 # Create JDBC resource referencing the pool
-asadmin create-jdbc-resource \
+asadmin --user admin --password-file=/opt/payara/password.txt create-jdbc-resource \
     --connectionpoolid ContactPool \
     jdbc/ContactDS
