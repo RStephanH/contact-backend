8f1d137cfb2c3b2d255c83569165a207285a77a2
rsh <randrianirinahernandezstephan@gmail.com>
2025-10-05 22:56:58 +0300

    feat: trust certificate

--- config/setup-jdbc.sh
+++ config/setup-jdbc.sh
@@ -4,16 +4,16 @@
 sleep 20
 echo "AS_ADMIN_PASSWORD=admin">>password.txt
 
-asadmin add-library --user admin --passwordfile=/opt/payara/password.txt ./mariadb-java-client-3.5.5.jar
+asadmin add-library --user admin --passwordfile=/opt/payara/password.txt ./mariadb-java-client-3.5.5.jar --trust-all
 
 # Create JDBC connection pool
 asadmin --user admin --passwordfile=/opt/payara/password.txt create-jdbc-connection-pool \
     --restype=javax.sql.DataSource \
     --datasourceclassname=org.mariadb.jdbc.MariaDbDataSource \
     --property user=contactuser:password=contactpass:url=jdbc\\:mariadb\\://mariadb\\:3306/contactdb \
-    ContactPool
+    ContactPool --trust-all
 
 # Create JDBC resource referencing the pool
 asadmin --user admin --passwordfile=/opt/payara/password.txt create-jdbc-resource \
     --connectionpoolid ContactPool \
-    jdbc/ContactDS
+    jdbc/ContactDS --trust-all
